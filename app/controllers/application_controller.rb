7596fb5592ff4a91ff157423606fb0296021fc38
Nick Paolino <github email address>
2017-10-10 17:14:50 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,21 @@
 require 'sinatra/base'
+require 'pry'
 
 class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get '/' do
+      erb :super_hero
+    end
 
+    post '/teams' do
+      @team = Team.new(params["team"])
+      params.select {|param, data| param != "team"}.each do |member, details|
+        Superhero.new(details)
+      end
+
+      @superheroes = Superhero.all
+      erb :team
+    end
 end
---
+++ app/models/super_hero.rb
@@ -0,0 +1,16 @@
+class Superhero
+  attr_accessor :name, :power, :biography
+
+  @@all = []
+
+  def initialize(params)
+    @name = params[:name]
+    @power = params[:power]
+    @biography = params[:biography]
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+end
